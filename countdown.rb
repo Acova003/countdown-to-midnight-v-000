47242e8c185267c193fd71bfb2373fe31468947d
Amee Covarrubias <ameecova@yahoo.com>
2018-07-19 23:57:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,11 @@
 #write your code here
 
-def countdown(int)
-  counter = 0 
+def countdown(number)
+  while number > 0 
+    number -= 1 
+    puts "#{number} SECOND(S)!"
+  end
+  puts "HAPPY NEW YEAR! "
 end
 
 def countdown_with_sleep
