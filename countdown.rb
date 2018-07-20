baa1224603610af07040c8a7fd9d6f90762b6d3a
Amee Covarrubias <ameecova@yahoo.com>
2018-07-20 00:32:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown(number)
   number = number.to_i
   while number > 0 
-   puts "#{number} SECOND(S)!"
-   
+    puts "#{number} SECOND(S)!"
+    number -= 1 
   end
   puts "HAPPY NEW YEAR! "
 end
